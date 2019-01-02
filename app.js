8f1ada582bfba2008cef2a3b91d456e397718251
Ashish Subedi <ashishsubedi10@outlook.com>
2019-01-02 22:42:14 +0545

    Handled google auth

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+//Passport config
+require('./config/passport')(passport);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+//Routes
+const auth = require('./routes/auth');
 
 
 app.get('/',(req,res)=>{
     res.send("WORKING");
 });
+
+app.use('/auth',auth);
 
 app.listen(PORT,console.log(`Server started at ${PORT}`));
---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports={
+    mongoURI : 'mongodb://admin:admin123@ds247674.mlab.com:47674/storyboard',
+    googleClientID:
+    '404600200065-tq30gfm130pmpueh040e15eg3vb84sm4.apps.googleusercontent.com',
+    googleClientSecret: 'HwPL-oMYf_yDUJOATHpuihx6'
+
+}
