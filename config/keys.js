7399f6f5748ee9b53c50f2f52f4d9801c5c24464
Ashish Subedi <ashishsubedi10@outlook.com>
2019-01-03 13:19:12 +0545

    Deployment version 1.0.0alpha done

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,6 @@
-module.exports={
-    mongoURI : 'mongodb://admin:admin123@ds247674.mlab.com:47674/storyboard',
-    googleClientID:
-    '404600200065-tq30gfm130pmpueh040e15eg3vb84sm4.apps.googleusercontent.com',
-    googleClientSecret: 'HwPL-oMYf_yDUJOATHpuihx6'
+if(process.env.NODE_ENV === 'production'){
+    module.exports = require('./keys_prod');
+}else{
+    module.exports = require('./keys_dev');
 
 }
